e78827e093ba2f3f78ef6c364f70898d11856be8
YanSong <yan.song@ia.ac.cn>
2023-09-15 14:47:59 +0800

    update ReadME

--- train_light_malib.sh
+++ train_light_malib.sh
@@ -1,4 +1,7 @@
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" &> /dev/null && pwd)
+export PYTHONPATH=$SCRIPT_DIR
+
 #python light_malib/main_pbt.py --config expr_configs/magent_battle/expr_dqn_vs_random_marl.yaml
-#python light_malib/main_pbt.py --config expr_configs/leduc_poker/expr_dqn_psro.yaml
+python light_malib/main_pbt.py --config expr_configs/leduc_poker/expr_dqn_psro.yaml
 #python light_malib/main_pbt.py --config expr_configs/magent_battle/expr_dqn_vs_attack_marl.yaml
-python light_malib/main_pbt.py --config expr_configs/magent_battle/expr_dqn_vs_attack_marl.yaml
+#python light_malib/main_pbt.py --config expr_configs/magent_battle/expr_dqn_vs_attack_marl.yaml
